94e60b7c5ffa989ab920646ed277cbb714cbfe80
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-10-21 17:10:15 +0900

    10.21
    .

---
+++ book/src/main/java/net/skhu/dto/Book.java
@@ -0,0 +1,69 @@
+package net.skhu.dto;
+
+public class Book {
+
+	int id;
+	String title;
+	String author;
+	int categoryId;
+	int price;
+	int publisherId;
+	int available;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(int publisherId) {
+		this.publisherId = publisherId;
+	}
+
+	public int getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+
+}
