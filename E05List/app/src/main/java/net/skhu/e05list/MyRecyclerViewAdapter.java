b6e75205d7243c6732054f914e2c6bd10e61ec56
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-12-07 17:03:27 +0900

    1207
    .

---
+++ E05List/app/src/main/java/net/skhu/e05list/MyRecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package net.skhu.e05list;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    class ViewHolder extends RecyclerView.ViewHolder  implements CompoundButton.OnCheckedChangeListener {
+        TextView textView1, textView2;
+        CheckBox checkBox;
+
+        public ViewHolder(View view, ArrayList<Item> arrayList) {
+            super(view);
+            this.textView1 = view.findViewById(R.id.textView1);
+            this.textView2 = view.findViewById(R.id.textView2);
+            this.checkBox = view.findViewById(R.id.checkBox);
+            this.checkBox.setOnCheckedChangeListener(this);
+        }
+
+        public void setData() {
+            Item item = arrayList.get(super.getAdapterPosition());
+            this.textView1.setText(item.getTitle());
+            this.textView2.setText(item.getCreateTimeFormatted());
+            this.checkBox.setChecked(item.isChecked());
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            Item item = arrayList.get(super.getAdapterPosition());
+            item.setChecked(isChecked);
+            if(isChecked) ++checkedItemCount; else --checkedItemCount;
+            if(checkedItemCount<= 1)
+                ((Activity)textView1.getContext()).invalidateOptionsMenu();
+        }
+    }
+
+    LayoutInflater layoutInflater;
+    ArrayList<Item> arrayList;
+    int checkedItemCount = 0;
+
+    public MyRecyclerViewAdapter(Context context, ArrayList<Item> arrayList) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item, viewGroup, false);
+        return new ViewHolder(view, arrayList);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int index) {
+        viewHolder.setData();
+    }
+}
