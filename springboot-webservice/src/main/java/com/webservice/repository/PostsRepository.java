dcb842865882f543e03afb18aa18b2971dc3c7aa
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2019-01-01 20:36:34 +0900

    0101
    인텔리j로 다시 시작

--- springboot-webservice/src/main/java/com/webservice/repository/PostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.webservice.domain.Posts;
-
-public interface PostsRepository extends JpaRepository<Posts, Long>{
-
-}
