f7f58b0e3d44fb59a0790a1d7c930763110ae2ce
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2019-01-02 11:44:09 +0900

    0102
    .

--- springboot-webservice/src/main/java/com/webservice/service/PostsService.java
+++ springboot-webservice/src/main/java/com/webservice/service/PostsService.java
@@ -1,11 +1,14 @@
 package com.webservice.service;
 
+import com.webservice.dto.PostsMainResponseDto;
 import com.webservice.dto.PostsSaveRequestDto;
 import com.webservice.repository.PostsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -16,4 +19,13 @@ public class PostsService {
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
     }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
 }
