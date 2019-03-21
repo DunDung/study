c17549e7dcef4188d76c7e63a5b7d70f279a87ed
DunDung <ebseud6135@naver.com>
2019-03-21 22:54:25 +0900

    20190321
    vue.js와 spring연동 예제

---
+++ vue-spring-example/src/main/java/com/vuespringexample/vo/ResponseVO.java
@@ -0,0 +1,10 @@
+package com.vuespringexample.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseVO<T> {
+    private String message;
+    private boolean check = true;
+    private T response;
+}
---
+++ vue-spring-example/src/main/java/com/vuespringexample/vo/UserVO.java
@@ -0,0 +1,19 @@
+package com.vuespringexample.vo;
+
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+    private String name;
+    private String phone;
+
+    public UserVO(){}
+    public UserVO(String name, String phone) {
+        super();
+        this.name = name;
+        this.phone = phone;
+    }
+
+}
