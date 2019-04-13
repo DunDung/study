ac557e1fe953a8b7a379fc7fa66a9f91d3e0788d
DunDung <ebseud6135@naver.com>
2019-04-13 21:40:21 +0900

    20190413

---
+++ test/src/test.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class test {
+
+	public static void main(String[] args) {
+		int a = 5;
+		String s = "5";
+		int b = 6;
+		Integer.toOctalString(a);
+		System.out.println(Integer.toBinaryString(~a));
+		System.out.println(~a);
+	}
+
+}
+
