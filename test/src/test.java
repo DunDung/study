8d831b03cf788da2226548cf9aee868cbe3fb7cc
DunDung <ebseud6135@naver.com>
2019-04-21 23:42:51 +0900

    20190421

--- test/src/test.java
+++ test/src/test.java
@@ -1,15 +1,24 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-		int a = 5;
-		String s = "5";
-		int b = 6;
-		Integer.toOctalString(a);
-		System.out.println(Integer.toBinaryString(~a));
-		System.out.println(~a);
-	}
+		Scanner scan = new Scanner(System.in);
+		int [] a = new int[3];
+		int [] b = new int[2];
 
+		for(int i=0; i<3; i++)
+			a[i] = scan.nextInt();
+		for(int i=0; i<2; i++)
+			b[i] = scan.nextInt();
+
+		Arrays.sort(a);
+		Arrays.sort(b);
+		System.out.println(a[0]+b[0]-50);
+	}
 }
+
+
+
 
