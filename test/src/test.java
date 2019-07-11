7c006c365c4801217f3929dbc693118d4a0988cf
DunDung <ebseud6135@naver.com>
2019-07-12 01:15:01 +0900

    20190712

--- test/src/test.java
+++ test/src/test.java
@@ -1,12 +1,17 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class test {
 
-	public static void main(String[] args) throws IOException {
-		for(int i=0; i<3; i++) {
-			if(((int)i&1)>0)
-				System.out.println(i&1);
-		}
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.add(3);
+		pq.add(2);
+		pq.add(5);
+		System.out.println(pq.toString());
+		
 	}
 }
