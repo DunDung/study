044687130de1637596c6a8b43ebddb3772c5fb8a
DunDung <ebseud6135@naver.com>
2019-08-28 15:40:07 +0900

    20190828

--- mybatis0/src/main/java/net/skhu/Mybatis0Application.java
+++ mybatis0/src/main/java/net/skhu/Mybatis0Application.java
@@ -1,9 +1,12 @@
 package net.skhu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Mybatis0Application {
 
     public static void main(String[] args) {
