d79c06714daa24582fc2681b26e1d45e9e2c55ad
DunDung <ebseud6135@naver.com>
2019-09-18 21:27:48 +0900

    20190918

--- bootstrap_board/src/components/Board.vue
+++ bootstrap_board/src/components/Board.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <!--fields로 필요한 속성만 보여주기, 컬럼명 변경 -->
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
   </div>
 </template>
 
 <script>
 import data from '@/data'
 
-//글을 최신순으로 정렬하기
-let items = data.Content.sort((a, b) => {return b.content_id - a.content_id})
-//map함수로 작성자 추가하기 ...전개연산자로 객체 모두 받아오고 user_name속성 추가
-//filter 함수는 조건에 맞는 것만 판별해서 리턴한다.
-items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id == contentItem.user_id)[0].name}})
 
   export default {
     data() {
+      //글을 최신순으로 정렬하기
+      let items = data.Content.sort((a, b) => {return b.content_id - a.content_id})
+      //map함수로 작성자 추가하기 ...전개연산자로 객체 모두 받아오고 user_name속성 추가
+      //filter 함수는 조건에 맞는 것만 판별해서 리턴한다.
+      items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id == contentItem.user_id)[0].name}})
       return {
         fields: [
           {
@@ -36,6 +36,14 @@ items = items.map(contentItem => {return {...contentItem, user_name: data.User.f
           }
         ],
         items: items
+      }
+    },
+    methods: {
+      rowClick(item, index, e){
+        this.$router.push({
+          //``와 ''의 차이는?
+          path: `/board/free/detail/${item.content_id}`
+        })
       }
     }
   }
--- bootstrap_board/src/data/index.js
+++ bootstrap_board/src/data/index.js
@@ -2,27 +2,27 @@ export default {
   User: [
     {
       user_id: 1,
-      name: '홍준성',
-      created_at: '2019-09-11 03:58:11'
+      name: 'lelana',
+      created_at: '2018-09-11 11:42:11'
     },
     {
       user_id: 2,
       name: '아이린',
-      created_at: '2019-09-11 03:58:11'
+      created_at: '2018-09-11 11:42:11'
     },
     {
       user_id: 3,
       name: '조이',
-      created_at: '2019-09-11 03:58:11'
+      created_at: '2018-09-11 11:42:11'
     },
   ],
   Content: [
     {
       content_id: 1,
       user_id: 1,
-      title: '홍준성의 강의 알람표',
-      context: '아직 미정입니다,',
-      created_at: '2019-09-11 03:58:11',
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
       updated_at: null
     },
     {
@@ -30,7 +30,7 @@ export default {
       user_id: 3,
       title: '레드벨벳 많이 사랑해 주세요^^',
       context: '화이팅!',
-      created_at: '2019-09-11 03:58:11',
+      created_at: '2019-01-02 13:11:42',
       updated_at: null
     },
     {
@@ -38,9 +38,9 @@ export default {
       user_id: 2,
       title: '생일 축하해주신 여러분 감사합니다!',
       context: '^^',
-      created_at: '2019-09-11 03:58:11',
+      created_at: '2019-03-29 13:11:42',
       updated_at: null
-    },
+    }
   ],
   Comment: [
     {
@@ -48,7 +48,7 @@ export default {
       user_id: 1,
       content_id: 3,
       context: '생일 축하해요!',
-      created_at: '2019-09-12 10:13:12',
+      created_at: '2019-03-29 14:11:11',
       updated_at: null
     },
     {
@@ -56,15 +56,15 @@ export default {
       user_id: 3,
       content_id: 3,
       context: '주현언니 생일 축하해요!',
-      created_at: '2019-09-12 10:13:12',
+      created_at: '2019-03-29 16:11:11',
       updated_at: null
     },
     {
       comment_id: 3,
       user_id: 2,
       content_id: 1,
-      context: '강의 잘보고 있습니다.',
-      created_at: '2019-09-12 10:13:12',
+      context: '강의 잘 보고 있습니다 ^^',
+      created_at: '2019-03-29 14:11:11',
       updated_at: null
     }
   ],
@@ -73,8 +73,8 @@ export default {
       subcomment_id: 1,
       comment_id: 3,
       user_id: 1,
-      context: '오 아이린님이 직접 댓글을 달아주시다니 ㅠ',
-      created_at: '2019-09-12 10:23:12',
+      context: '오 아이린님이 직접 댓글을 달아주시다니 ㅠㅠ 영광입니다!!',
+      created_at: '2019-03-29 16:22:11',
       updated_at: null
     }
   ]
