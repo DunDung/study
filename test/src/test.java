6be284031be9cf779909938b2e5bf9176ec10f00
DunDung <ebseud6135@naver.com>
2019-10-08 20:52:24 +0900

    20191008

--- test/src/test.java
+++ test/src/test.java
@@ -1,17 +1,20 @@
-
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class test {
-
+	static int a,b,c;
 	public static void main(String[] args) {
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.add(3);
-		pq.add(2);
-		pq.add(5);
-		System.out.println(pq.toString());
-		
+		for(int i=0; i<10; i++) {
+			double v = (Math.random()) ;
+			System.out.println(v);
+		}
 	}
+	static void prnt() {
+		while(a < b) {
+			System.out.println(a);
+			++a;
+			c= c+a;
+			prnt();
+		}
+	}
+	
 }
