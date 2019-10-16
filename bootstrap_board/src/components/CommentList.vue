2f6b08c6540ff25c53a83b3aa6eb98d93d3207f9
DunDung <ebseud6135@naver.com>
2019-10-17 03:34:58 +0900

    20191017

--- bootstrap_board/src/components/CommentList.vue
+++ bootstrap_board/src/components/CommentList.vue
@@ -3,16 +3,19 @@
     <div :key="item.comment_id" v-for="item in comments">
       <CommentListItem :commentObj="item"/>
     </div>
+    <CommentCreate :contentId="contentId" :reloadComment="reloadComment"/>
   </div>
 </template>
 <script>
 import data from '@/data';
 import CommentListItem from './CommentListItem';
+import CommentCreate from './CommentCreate';
 
 export default {
   name : 'CommentList',
   components: {
-    CommentListItem
+    CommentListItem,
+    CommentCreate,
   },
   props: {
     contentId: Number,
@@ -21,6 +24,11 @@ export default {
   data() {
     return {
       comments: data.Comment.filter(item => item.content_id === this.contentId)
+    }
+  },
+  methods: {
+    reloadComment() {
+      this.comments = data.Comment.filter(item => item.content_id === this.contentId)
     }
   }
 }
