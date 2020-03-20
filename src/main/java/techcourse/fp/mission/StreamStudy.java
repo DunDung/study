ada2b0ad5c61c3c22a9d02c36730bac9ddf5e2ce
JunSeong <ebseud6135@naver.com>
2020-03-20 13:56:36 +0900

    [Feat] Stream API를 이용하여 파일내의 문자를 조건에맞게 추출

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -6,7 +6,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StreamStudy {
     public static long countWords() throws IOException {
@@ -41,7 +44,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return numbers.stream().filter(v -> v > 3).mapToInt(v -> v * 2).sum();
+        return numbers.stream()
+            .filter(v -> v > 3)
+            .mapToInt(v -> v * 2).sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -49,8 +54,12 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        System.out.println(words);
-        System.out.println(words.size());
-        // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(word -> word.length() > 12)
+            .limit(100)
+            .sorted(Comparator.comparing(String::length).reversed())
+            .distinct()
+            .map(String::toLowerCase)
+            .forEach(System.out::println);
     }
 }
