ecfab4dfc19f826ed99f102f70e9eb7eb621ad58
JunSeong <ebseud6135@naver.com>
2020-07-16 21:37:48 +0900

    feat : Reflection 예제 추가

---
+++ blog_example/reflection/src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public void move() {
+        this.position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ blog_example/reflection/src/Example.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Method;
+
+public class Example {
+
+    public static void main(String[] args) throws Exception {
+        Object obj = new Car("foo", 0);
+        Class carClass = Class.forName("Car");
+        Method move = carClass.getMethod("move");
+
+        // 메서드 실행, invoke(메서드를 실행시킬 객체, 해당 메서드에 넘길 인자)
+        move.invoke(obj, null);
+
+        Method getPosition = carClass.getMethod("getPosition");
+        int position = (int)getPosition.invoke(obj, null);
+        System.out.println(position);
+        // 출력 결과: 1
+    }
+}
