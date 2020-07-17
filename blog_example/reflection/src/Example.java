c7388b946467586d06840f0fec71f052c4ca4571
JunSeong <ebseud6135@naver.com>
2020-07-17 13:42:03 +0900

    Reflection 끝

--- blog_example/reflection/src/Example.java
+++ blog_example/reflection/src/Example.java
@@ -4,7 +4,7 @@ public class Example {
 
     public static void main(String[] args) throws Exception {
         Object obj = new Car("foo", 0);
-        Class carClass = Class.forName("Car");
+        Class carClass = obj.getClass();
         Method move = carClass.getMethod("move");
 
         // 메서드 실행, invoke(메서드를 실행시킬 객체, 해당 메서드에 넘길 인자)
