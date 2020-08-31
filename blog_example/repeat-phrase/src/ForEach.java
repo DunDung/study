2f4bba6296ed57bc3574c996fac1efa1d57d17d1
JunSeong <ebseud6135@naver.com>
2020-09-01 03:49:41 +0900

    feat : 반복문 예제 추가

---
+++ blog_example/repeat-phrase/src/ForEach.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class ForEach {
+    public static void main(String[] args) {
+        example2(new String[]{"a", "b", "c", "d"});
+    }
+
+    public static void example(List<String> words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void example2(String[] words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
